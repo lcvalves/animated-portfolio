597401485ad6e191b86a623ddcb3d1bc852b0b88
Luís Alves <luas@ipvc.pt>
2022-07-23 19:25:55 +0100

    🔻 Three.js `main.js` setup + Add background

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
 import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// Setup
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Background
+const spaceTexture = new THREE.TextureLoader().load(
+  "/public/carina-nebula.jpg"
+);
+scene.background = spaceTexture;
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
