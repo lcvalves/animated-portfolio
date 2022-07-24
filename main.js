7caaf4c002cc0bbbcdef35a61fa210098f5fd52d
Luís Alves <luas@ipvc.pt>
2022-07-24 18:26:33 +0100

    🔻 Add Galaxy GLTF model + Markup text

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Setup
@@ -9,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  2000
 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -18,34 +19,57 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setZ(100);
+camera.position.setX(0);
 
 renderer.render(scene, camera);
 
+// Background
+const nebula = new THREE.TextureLoader().load("/carina-nebula.jpg");
+scene.background = nebula;
+
 // Lights
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(0, 0, 0);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
+// Galaxy
+let loadedModel;
+const loader = new GLTFLoader();
+loader.load(
+  "/assets/galaxy/scene.gltf",
+  function (gltf) {
+    loadedModel = gltf;
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
 // Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(100, 100);
+const axesHelper = new THREE.AxesHelper(100);
+
+scene.add(lightHelper, gridHelper, axesHelper);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Background
-const spaceTexture = new THREE.TextureLoader().load(
-  "/public/carina-nebula.jpg"
-);
-scene.background = spaceTexture;
-
 // Animation Loop
-
 function animate() {
+  if (loadedModel) {
+    loadedModel.scene.scale.set(250, 250, 250);
+    loadedModel.scene.rotation.x -= 0.0025;
+    loadedModel.scene.rotation.y -= 0.0025;
+    loadedModel.scene.rotation.z += 0.001;
+  }
+
   requestAnimationFrame(animate);
-
   controls.update();
-
   renderer.render(scene, camera);
 }
 
