4fbef5937668852d3bf63fa5434f13934cff43a3
Luís Alves <luas@ipvc.pt>
2022-07-26 04:00:22 +0100

    🌍 Add earth model

--- main.js
+++ main.js
@@ -5,24 +5,33 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Setup
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
-
 renderer.render(scene, camera);
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 0);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(lightHelper, gridHelper, axesHelper);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Torus knot
 const geometry = new THREE.TorusKnotGeometry(30, 1.5, 300, 20, 7, 12);
@@ -47,10 +56,6 @@ function addStar() {
 }
 Array(500).fill().forEach(addStar);
 
-// Background
-const nebula = new THREE.TextureLoader().load("/carina-nebula.jpg");
-scene.background = nebula;
-
 // Avatar cube
 const cubeTexture = new THREE.TextureLoader().load("luis.png");
 const cube = new THREE.Mesh(
@@ -62,20 +67,192 @@ cube.scale.set(3, 3, 3);
 cube.position.z = 8;
 cube.position.x = 5;
 
-// Lights
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 0, 0);
+// Earth Proto
+let planetProto = {
+  sphere: function (size) {
+    let sphere = new THREE.SphereGeometry(size, 32, 32);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+    return sphere;
+  },
+  material: function (options) {
+    let material = new THREE.MeshPhongMaterial();
+    if (options) {
+      for (var property in options) {
+        material[property] = options[property];
+      }
+    }
+
+    return material;
+  },
+  glowMaterial: function (intensity, fade, color) {
+    // Custom glow shader from https://github.com/stemkoski/stemkoski.github.com/tree/master/Three.js
+    let glowMaterial = new THREE.ShaderMaterial({
+      uniforms: {
+        c: {
+          type: "f",
+          value: intensity,
+        },
+        p: {
+          type: "f",
+          value: fade,
+        },
+        glowColor: {
+          type: "c",
+          value: new THREE.Color(color),
+        },
+        viewVector: {
+          type: "v3",
+          value: camera.position,
+        },
+      },
+      vertexShader: `
+        uniform vec3 viewVector;
+        uniform float c;
+        uniform float p;
+        varying float intensity;
+        void main() {
+          vec3 vNormal = normalize( normalMatrix * normal );
+          vec3 vNormel = normalize( normalMatrix * viewVector );
+          intensity = pow( c - dot(vNormal, vNormel), p );
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        }`,
+      fragmentShader: `
+        uniform vec3 glowColor;
+        varying float intensity;
+        void main() 
+        {
+          vec3 glow = glowColor * intensity;
+          gl_FragColor = vec4( glow, 1.0 );
+        }`,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+    });
+
+    return glowMaterial;
+  },
+  texture: function (material, property, uri) {
+    let textureLoader = new THREE.TextureLoader();
+    textureLoader.crossOrigin = true;
+    textureLoader.load(uri, function (texture) {
+      material[property] = texture;
+      material.needsUpdate = true;
+    });
+  },
+};
+
+let createPlanet = function (options) {
+  // Create the planet's Surface
+  let surfaceGeometry = planetProto.sphere(options.surface.size);
+  let surfaceMaterial = planetProto.material(options.surface.material);
+  let surface = new THREE.Mesh(surfaceGeometry, surfaceMaterial);
+
+  // Create the planet's Atmosphere
+  let atmosphereGeometry = planetProto.sphere(
+    options.surface.size + options.atmosphere.size
+  );
+  let atmosphereMaterialDefaults = {
+    side: THREE.DoubleSide,
+    transparent: true,
+  };
+  let atmosphereMaterialOptions = Object.assign(
+    atmosphereMaterialDefaults,
+    options.atmosphere.material
+  );
+  let atmosphereMaterial = planetProto.material(atmosphereMaterialOptions);
+  let atmosphere = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+
+  // Create the planet's Atmospheric glow
+  let atmosphericGlowGeometry = planetProto.sphere(
+    options.surface.size +
+      options.atmosphere.size +
+      options.atmosphere.glow.size
+  );
+  let atmosphericGlowMaterial = planetProto.glowMaterial(
+    options.atmosphere.glow.intensity,
+    options.atmosphere.glow.fade,
+    options.atmosphere.glow.color
+  );
+  let atmosphericGlow = new THREE.Mesh(
+    atmosphericGlowGeometry,
+    atmosphericGlowMaterial
+  );
+
+  // Nest the planet's Surface and Atmosphere into a planet object
+  let planet = new THREE.Object3D();
+  surface.name = "surface";
+  atmosphere.name = "atmosphere";
+  atmosphericGlow.name = "atmosphericGlow";
+  planet.add(surface);
+  planet.add(atmosphere);
+  planet.add(atmosphericGlow);
+
+  // Load the Surface's textures
+  for (let textureProperty in options.surface.textures) {
+    planetProto.texture(
+      surfaceMaterial,
+      textureProperty,
+      options.surface.textures[textureProperty]
+    );
+  }
+
+  // Load the Atmosphere's texture
+  for (let textureProperty in options.atmosphere.textures) {
+    planetProto.texture(
+      atmosphereMaterial,
+      textureProperty,
+      options.atmosphere.textures[textureProperty]
+    );
+  }
+
+  return planet;
+};
+
+let earth = createPlanet({
+  surface: {
+    size: 3,
+    material: {
+      bumpScale: 0.05,
+      specular: new THREE.Color("grey"),
+      shininess: 10,
+    },
+    textures: {
+      map: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthmap1k.jpg",
+      bumpMap:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthbump1k.jpg",
+      specularMap:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthspec1k.jpg",
+    },
+  },
+  atmosphere: {
+    size: 0.003,
+    material: {
+      opacity: 0.8,
+    },
+    textures: {
+      map: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthcloudmap.jpg",
+      alphaMap:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthcloudmaptrans.jpg",
+    },
+    glow: {
+      size: 0.02,
+      intensity: 0.7,
+      fade: 7,
+      color: 0x93cfef,
+    },
+  },
+});
+scene.add(earth);
+earth.position.z = 30;
+earth.position.setX(-10);
 
 // Galaxy
-let loadedModel;
+let galaxyModel;
 const loader = new GLTFLoader();
 loader.load(
   "/assets/galaxy/scene.gltf",
   function (gltf) {
-    loadedModel = gltf;
+    galaxyModel = gltf;
     scene.add(gltf.scene);
   },
   undefined,
@@ -84,23 +261,18 @@ loader.load(
   }
 );
 
-// Helpers
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-const axesHelper = new THREE.AxesHelper(100);
-
-scene.add(lightHelper, gridHelper, axesHelper);
-
-const controls = new OrbitControls(camera, renderer.domElement);
+// Background
+const nebula = new THREE.TextureLoader().load("/carina-nebula.jpg");
+scene.background = nebula;
 
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  if (loadedModel) {
-    loadedModel.scene.scale.set(50, 50, 50);
-    loadedModel.scene.rotation.x -= 0.0025;
-    loadedModel.scene.rotation.y -= 0.0025;
-    loadedModel.scene.rotation.z += 0.001;
+  if (galaxyModel) {
+    galaxyModel.scene.scale.set(50, 50, 50);
+    galaxyModel.scene.rotation.x -= 0.0025;
+    galaxyModel.scene.rotation.y -= 0.0025;
+    galaxyModel.scene.rotation.z += 0.001;
   }
   controls.update();
   renderer.render(scene, camera);
